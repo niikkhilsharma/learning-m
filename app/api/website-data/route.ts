b640e5de6361a2ecb188cfac3ca495ea9535c655
Murali <murali.sid@fn7.io>
2026-01-23 00:39:55 +0530

    testing jsdom

---
+++ app/api/website-data/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import * as z from "zod";
+import axios, { AxiosError } from "axios";
+import { JSDOM } from "jsdom";
+
+const websiteUrlSchema = z.object({
+  websiteUrl: z.string().url(),
+});
+
+// const metadata = {
+//   title: document.querySelector("title").textContent,
+//   description: document.querySelector('meta[name="description"]').getAttribute("content"),
+//   ogImage: document.querySelector('meta[property="og:image"]').getAttribute("content"),
+// };
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const websiteUrl = searchParams.get("websiteUrl");
+
+  try {
+    const validatedWebsiteUrl = websiteUrlSchema.parse({ websiteUrl });
+    const { data } = await axios.get(validatedWebsiteUrl.websiteUrl);
+    const dom = new JSDOM(data);
+    const document = dom.window.document;
+    const metadata = {
+      title: document.querySelector("title")?.textContent,
+      description: document.querySelector('meta[name="description"]')?.getAttribute("content"),
+      ogImage: document.querySelector('meta[property="og:image"]')?.getAttribute("content"),
+    };
+
+    return NextResponse.json({ metadata, websiteUrl }, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: "Invalid website URL", details: error.issues.map((issue) => issue.message).join(", ") },
+        { status: 400 }
+      );
+    }
+    if (error instanceof AxiosError) {
+      return NextResponse.json({ error: "Failed to get website data", details: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: "Failed to get website data", details: error }, { status: 500 });
+  }
+}
